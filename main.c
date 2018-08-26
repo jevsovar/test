39f4a2f309cf04a55b23729411cb676161ba5e0c
JanJ <jan.jevsovar@gmail.com>
2018-08-26 07:09:48 +0200

    začel nek poskus z zanko

--- main.c
+++ main.c
@@ -4,7 +4,11 @@
 
 int main()
 {
-    printf("Hello beautiful world.");
-    printf("Hello all back there!");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Hello beautiful world.");
+        printf("Hello all back there!");
+    }
+    
     return 0;
 }
